5f917a3e9dfe0460ea0f8528402ac41f548a4b6b
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-07-05 20:49:21 +0200

    Feat : Reverting back to seperate functions

--- Login_Hash_System.py
+++ Login_Hash_System.py
@@ -24,6 +24,33 @@ def CreateAccount():
     with open(FILE_NAME, "a") as file:
         file.write(f"{username}:{password_hash}\n")
 
+    print("\nAccount created successfully :)")
 
+# _________________________________________________________________________________________________
+
+def login():
+
+    print("\n=== Login ===")
+
+    username = input("Username : ")
+    password = input("Password : ")
+
+    password_hash = hash_password(password)
+
+    if not os.path.exists(FILE_NAME):
+        print("\nNo users have been registered :(")
+        return
+
+    with open(FILE_NAME, "r") as file:
+
+        for line in file:
+
+            saved_username, saved_hash = line.strip().split(":")
+
+            if username == saved_username and password_hash == saved_hash:
+                print("\nLogin Successful :)")
+                return
+
+    print("\nInvalid username or password :(")
 
 # _________________________________________________________________________________________________
