3293e34d8304d9243ab0966d75cd6791e791804c
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-07-05 20:45:45 +0200

    Feat : Added Login Function

--- Login_Hash_System.py
+++ Login_Hash_System.py
@@ -3,6 +3,7 @@
 # =========================
 
 import hashlib
+import os
 
 FILE_NAME = "users.txt"
 
@@ -11,7 +12,7 @@ def hash_password(password):
 
 # _________________________________________________________________________________________________
 
-def register():
+def CreateAccount():
 
     print("\n=== Create Account ===")
 
@@ -24,5 +25,32 @@ def register():
         file.write(f"{username}:{password_hash}\n")
 
     print("\nAccount created successfully")
+
+# _________________________________________________________________________________________________
+
+def login():
+
+    print("\n=== Login ===")
+
+    username = input("Username : ")
+    password = input("Password : ")
+
+    password_hash = hash_password(password)
+
+    if not os.path.exists(FILE_NAME):
+        print("\nNo users have been registered.")
+        return
+
+    with open(FILE_NAME, "r") as file:
+
+        for line in file:
+
+            saved_username, saved_hash = line.strip().split(":")
+
+            if username == saved_username and password_hash == saved_hash:
+                print("\nLogin Successful!")
+                return
+
+    print("\nInvalid username or password.")
 
 # _________________________________________________________________________________________________
